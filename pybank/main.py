74e5828a9926a324c23565537e4ba2ba8a060ec4
ElephantsRBig <CarlosR03@gmail.com>
2019-06-12 09:54:47 -0400

    So here i am commenting and finishing off little details to make 	the code a little more clearner and presentable. A lot of 	comments were made.

--- pybank/main.py
+++ pybank/main.py
@@ -1,58 +1,54 @@
+# Dependencies
 import os
 import csv
 
-
+# Creating path to read the file
 csv_file = os.path.join('pybank.csv')
 
-total_months = 0 #Total months
-sum_of_PL = 0 #The sume of profits and loses
-changes = 0
-differences = []
-maxvalue = 0
-minvalue = 0
+# Declaring some variables out of habit
+total_months = 0            # total months
+sum_of_PL = 0               # the sume of profits and loses
+changes = 0                 # the changes between two month
+differences = []            # where the appended changes of revenue between months is added to
+maxvalue = 0                # the max increase of change in profits
+minvalue = 0                # the max decrease of change in profits
+revenue =0                  # reference variable for appending values to differences, revenue for the month
 
-revenue =0
-
+# Opening and reading the file
 with open(csv_file,newline ='') as csvfile:
     csvreader = csv.reader(csvfile,delimiter=",")
     
     csv_header = next(csvreader)
-    for row in csvreader:
-        total_months=total_months+1
-        sum_of_PL = sum_of_PL + int(row[1])
-        changes = int(row[1]) - (revenue)
-        differences.append(changes)
+    for row in csvreader:                       # looping through the csvfile
+        total_months=total_months+1             # finding the total months in the csvfile
+        sum_of_PL = sum_of_PL + int(row[1])     # finding the sum of the profits/loses
+        changes = int(row[1]) - (revenue)       # determining the changes between months
+        differences.append(changes)             # appending the differences to a list for further calculation
+        revenue = int(row[1])                   # using this as reference to find the differences
+
+        # If the change is the largest increase/decrease, save it and comparing against other values
+        # Also saving the dates for each time we receive the largest increase/decrease
         if changes > maxvalue :
-            maxvalue=changes
-            maxvaluedate=row[0]
+            maxvalue=changes            # saving the value for the largest increase in profits
+            maxvaluedate=row[0]         # the date for the largest increase in profits
         
         if changes < minvalue :
-            minvalue = changes
-            minvaluedate=row[0]
+            minvalue = changes          # savings the value for the largest decrease in profits 
+            minvaluedate=row[0]         # the date for the largest decrease in profits
         
 
+        
 
-
-
-        revenue = int(row[1])
-
-print(sum_of_PL)
-print(revenue)
-print(changes)
-print(differences)
-
+# stating the average of changes
 change = sum(differences[1:])/(total_months-1)
 
-print(change)
-print(maxvalue)
-print(minvalue)
-print(maxvaluedate)
-print(minvaluedate)
-
+# Printing the results
+print('-------------------------------')
 print('Financial Analysis')
 print('-------------------------------')
 print(f'Total Months: {total_months}')
 print(f'Total: {sum_of_PL}')
-print(f'Average Change: ${change}')
+print(f'Average Change: ${round(change,2)}')
 print(f'Greatest Increase in Profits: {maxvaluedate} ({maxvalue})')
 print(f'Greatest Decrease in Profits: {minvaluedate} ({minvalue})')
+print('-------------------------------')
